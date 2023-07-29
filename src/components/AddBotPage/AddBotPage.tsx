dab946c29c32814fd9eb7cfda9f4ecaeecdf693f
randomu3 <demiz99@mail.ru>
2023-07-29 23:35:13 +0500

    added notification component;

--- src/components/AddBotPage/AddBotPage.tsx
+++ src/components/AddBotPage/AddBotPage.tsx
@@ -1,8 +1,10 @@
+import Notification from '../Notification/Notification';
 import './AddBotPage.scss';
 
 function AddBotPage() {
     return (
         <section>
+        <Notification></Notification>
         </section>
     );
 }
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import image from './images/Component 29.png';
+import './Notification.scss';
+
+const Notification = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  const handleOpen = () => {
+    setIsVisible(true);
+  };
+
+  return (
+    <>
+      {/* Опциональна9 кнопка дл9 отображени9*/}
+      <button onClick={handleOpen}>Показать уведомление</button>
+      <div className={`notification__popup ${isVisible ? 'notification__popup--visible' : ''}`}>
+        <p className="notification__message">
+          Письмо с подтверждением отправлено тебе на <span className="notification__email">/email!</span>
+        </p>
+        <img src={image} className="notification__image" alt="Бот" />
+        <button className="notification__close-button" onClick={handleClose}>x</button>
+      </div>
+    </>
+  );
+};
+
+export default Notification;
