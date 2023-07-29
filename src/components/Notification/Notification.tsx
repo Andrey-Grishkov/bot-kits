6f5c5a7a2e45bd102bb07f67cdba32f6636e0e41
randomu3 <demiz99@mail.ru>
2023-07-30 00:07:57 +0500

    added some styles for noti

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -19,7 +19,7 @@ const Notification = () => {
       <button onClick={handleOpen}>Показать уведомление</button>
       <div className={`notification__popup ${isVisible ? 'notification__popup--visible' : ''}`}>
         <p className="notification__message">
-          Письмо с подтверждением отправлено тебе на <span className="notification__email">/email!</span>
+          Письмо с подтверждением отправлено тебе на <span className="notification__email">/email!</span>!
         </p>
         <img src={image} className="notification__image" alt="Бот" />
         <button className="notification__close-button" onClick={handleClose}>x</button>
