96d4a716efd9484138b4967a44c14e7910a6429c
randomu3 <demiz99@mail.ru>
2023-07-31 22:25:58 +0500

    fix notific after review

--- src/components/AddBotPage/AddBotPage.tsx
+++ src/components/AddBotPage/AddBotPage.tsx
@@ -1,12 +1,9 @@
-import Notification from '../Notification/Notification';
 import './AddBotPage.scss';
 
 function AddBotPage() {
-    const handleOpen = () => true;
 
     return (
         <section>
-            <Notification imageType='password' handleOpen={handleOpen}></Notification>
         </section>
     );
 }
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -5,34 +5,34 @@ import passwordImage from './images/password.png';
 
 interface NotificationProps {
   handleOpen: () => boolean;
-  imageType?: 'letter' | 'password';
+  imageType: 'letter' | 'password';
 }
 
-const Notification: FC<NotificationProps> = ({ handleOpen, imageType }) => {
-  const [isVisible, setIsVisible] = useState(true);
+const Notification: FC<NotificationProps> = ({ handleOpen, imageType = 'letter' }) => {
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleClose = () => {
     setIsVisible(false);
   };
 
   useEffect(() => {
-    handleOpen();
+    setIsVisible(true);
   }, [handleOpen]);
 
-  let selectedImage;
-  if (imageType === 'letter') {
-    selectedImage = letterImage;
-  } else if (imageType === 'password') {
-    selectedImage = passwordImage;
-  }
+  const selectedImage = imageType === 'letter' ? letterImage : passwordImage;
+
+  const messageText = imageType === 'letter' 
+    ? 'Письмо с подтверждением отправлено тебе на' 
+    : 'Ссылка для сброса пароля отправлена тебе на';
 
   return (
     <div className={`notification__popup ${isVisible ? 'notification__popup--visible' : ''}`}>
-      <div className="notification__message">
-        {imageType === 'letter' ? 'Письмо с подтверждением отправлено тебе на' : 'Ссылка для сброса пароля отправлена тебе на'}<div className="notification__email">/email!</div>!
+      <div className="notification__popup__message">
+        {messageText}
+        <div className="notification__popup__email">/email!</div>!
       </div>
-        <img src={selectedImage} className="notification__image" alt="Уведомление" />
-      <div className="notification__close-button" onClick={handleClose}></div>
+      <img src={selectedImage} className="notification__popup__image" alt="Уведомление" />
+      <button className="notification__popup__close-button" onClick={handleClose} />
     </div>
   );
 };
