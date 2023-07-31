1c40ac058aaf586aa09a705881f7b0754208bd99
randomu3 <demiz99@mail.ru>
2023-07-31 13:02:13 +0500

    added notification;

--- src/components/AddBotPage/AddBotPage.tsx
+++ src/components/AddBotPage/AddBotPage.tsx
@@ -1,10 +1,8 @@
-import Notification from '../Notification/Notification';
 import './AddBotPage.scss';
 
 function AddBotPage() {
     return (
         <section>
-            <Notification></Notification>
         </section>
     );
 }
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,30 +1,32 @@
-import React, { useState } from 'react';
-import image from './images/Component 29.png';
+import React, { useState, FC, useEffect } from 'react';
+import image from './images/bot-kit.png';
 import './Notification.scss';
 
-const Notification = () => {
+interface NotificationProps {
+  text: string;
+  handleOpen: () => void;
+}
+
+const Notification: FC<NotificationProps> = ({ text, handleOpen }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const handleClose = () => {
     setIsVisible(false);
   };
 
-  const handleOpen = () => {
-    setIsVisible(true);
-  };
+  // Эффект открытия уведомления
+  useEffect(() => {
+    handleOpen();
+  }, [handleOpen]);
 
   return (
-    <>
-      {/* Опциональна9 кнопка дл9 отображени9*/}
-      <button onClick={handleOpen}>Показать уведомление</button>
-      <div className={`notification__popup ${isVisible ? 'notification__popup--visible' : ''}`}>
-        <p className="notification__message">
-          Письмо с подтверждением отправлено тебе на <span className="notification__email">/email!</span>!
-        </p>
-        <img src={image} className="notification__image" alt="Бот" />
-        <button className="notification__close-button" onClick={handleClose}>x</button>
-      </div>
-    </>
+    <div className={`notification__popup ${isVisible ? 'notification__popup--visible' : ''}`}>
+      <p className="notification__message">
+        {text} <span className="notification__email">/email!</span>!
+      </p>
+      <img src={image} className="notification__image" alt="Бот" />
+      <div className="notification__close-button" onClick={handleClose}></div>
+    </div>
   );
 };
 
