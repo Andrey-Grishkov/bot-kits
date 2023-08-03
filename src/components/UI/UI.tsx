e59d1782bf16aba30c0f16c39506bc109fde0727
Andrey Grishkov <88451960+Andrey-Grishkov@users.noreply.github.com>
2023-08-03 10:38:18 +0300

    Revert "UI components/common"

--- src/components/UI/UI.tsx
+++ src/components/UI/UI.tsx
@@ -21,13 +21,6 @@ import { ConstructorVideoButton } from "./ConstructorVideoButton/ConstructorVide
 import { ConstructorLogicButton } from "./ConstructorLogicButton/ConstructorLogicButton";
 import { ConstructorBackground } from './ConstructorLogicBackGround/ConstructorLogicBackGround'
 import { ConstructorPhoneButton } from './ConstructorPhoneButton/ConstructorPhoneButton'
-import { UploadButton } from './UploadButton/UploadButton';
-import { CountriesSelect } from './CountriesSelect/CountriesSelect';
-import { NumberSelect } from './NumberSelect/NumberSelect';
-import { BotCard } from './BotCard/BotCard';
-import { DropdownRepeat } from './DropdownRepeat/DropdownRepeat';
-import { DropdownSettings } from './DropdownSettings/DropdownSettings';
-import { AddTo } from './AddTo/AddTo';
 
 export function UI() {
   return (
@@ -67,13 +60,6 @@ export function UI() {
       </ConstructorLogicButton>
       </ConstructorBackground>
       <ConstructorPhoneButton/>
-      <UploadButton size="M" />
-      <BotCard id="id" name="Салон красоты" type="telegram"/>
-      <CountriesSelect />
-      <NumberSelect />
-      <AddTo />
-      <DropdownRepeat />
-      <DropdownSettings />
     </section>
   );
 }
