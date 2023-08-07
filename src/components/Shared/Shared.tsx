5ada983e467abac815388958c96d86bbfcaf9914
randomu3 <demiz99@mail.ru>
2023-08-07 16:30:58 +0500

    added stable version 1.1 desktop

--- src/components/Shared/Shared.tsx
+++ src/components/Shared/Shared.tsx
@@ -19,7 +19,7 @@ const Shared = () => {
       setCopied(false);
     }, 2000); // уведомление исчезнет через 2 секунды
   };
-  
+
   return (
     <div className="partners">
       <div className="partners__header">
@@ -30,54 +30,47 @@ const Shared = () => {
         <div className="partners__ref-link">botkits.ru/?ref=12345</div>
         <div className="partners__ref-copy" onClick={copyToClipboard}>
           <img src={copyImg} alt="Скопировать" />
-          {isCopied && <div className="partners__copied-notice">Ссылка скопирована</div>}
+          {isCopied && (
+            <div className="partners__copied-notice">Ссылка скопирована</div>
+          )}
         </div>
       </div>
       <div className="partners__content">
         <div className="partners__stats">
           <h3>Статистика рефераллов</h3>
-          <table className="partners__table partners__table--stats">
-            <thead>
-              <tr>
-                <th>Перешли по ссылке</th>
-                <th>Регистраций</th>
-                <th>Оплата</th>
-                <th>Сумма</th>
-                <th>Комиссия</th>
-                <th>Выплачено</th>
-                <th>Вывод</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>28 человек</td>
-                <td>15</td>
-                <td className="paid">Оплачено</td>
-                <td>5 000 ₽</td>
-                <td>1 500 ₽</td>
-                <td>4 500 ₽</td>
-                <td>1 500 ₽</td>
-              </tr>
-              <tr>
-                <td>17 человек</td>
-                <td>8</td>
-                <td className="paid">Оплачено</td>
-                <td>15 000 ₽</td>
-                <td>4 500 ₽</td>
-                <td>8 200 ₽</td>
-                <td>4 500 ₽</td>
-              </tr>
-              <tr>
-                <td>10 человек</td>
-                <td>10</td>
-                <td className="paid">Оплачено</td>
-                <td>5 000 ₽</td>
-                <td>1 500 ₽</td>
-                <td>10 000 ₽</td>
-                <td>1 500 ₽</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="partners__table partners__table--stats">
+            <th>Перешли по ссылке</th>
+            <th>Регистраций</th>
+            <th>Оплата</th>
+            <th>Сумма</th>
+            <th>Комиссия</th>
+            <th>Выплачено</th>
+            <th>Вывод</th>
+
+            <td>28 человек</td>
+            <td>15</td>
+            <td className="paid">Оплачено</td>
+            <td>5 000 ₽</td>
+            <td>1 500 ₽</td>
+            <td>4 500 ₽</td>
+            <td>1 500 ₽</td>
+
+            <td>17 человек</td>
+            <td>8</td>
+            <td className="paid">Оплачено</td>
+            <td>15 000 ₽</td>
+            <td>4 500 ₽</td>
+            <td>8 200 ₽</td>
+            <td>4 500 ₽</td>
+
+            <td>10 человек</td>
+            <td>10</td>
+            <td className="paid">Оплачено</td>
+            <td>5 000 ₽</td>
+            <td>1 500 ₽</td>
+            <td>10 000 ₽</td>
+            <td>1 500 ₽</td>
+          </div>
         </div>
         <div className="partners__payouts">
           <div className="partners__payouts--title">
@@ -92,40 +85,31 @@ const Shared = () => {
             </button>
           </div>
           {isPayoutsVisible && (
-            <table className="partners__table partners__table--payouts">
-              <thead>
-                <tr>
-                  <th>Дата запроса</th>
-                  <th>Дата выплаты</th>
-                  <th>Акт</th>
-                  <th>Статус</th>
-                  <th>Сумма выплаты</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>05.07.22</td>
-                  <td>07.07.22</td>
-                  <td>Док.pdf</td>
-                  <td className="paid">Выплачено</td>
-                  <td>1 500 ₽</td>
-                </tr>
-                <tr>
-                  <td>02.07.22</td>
-                  <td>-</td>
-                  <td>Фото.png</td>
-                  <td className="processing">В обработке</td>
-                  <td>4 500 ₽</td>
-                </tr>
-                <tr>
-                  <td>27.06.22</td>
-                  <td>28.06.22</td>
-                  <td>Data.pdf</td>
-                  <td className="paid">Выплачено</td>
-                  <td>1 000 ₽</td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="partners__table partners__table--payouts">
+              <th>Дата запроса</th>
+              <th>Дата выплаты</th>
+              <th>Акт</th>
+              <th>Статус</th>
+              <th>Сумма выплаты</th>
+
+              <td>05.07.22</td>
+              <td>07.07.22</td>
+              <td className="act">Док.pdf</td>
+              <td className="paid">Выплачено</td>
+              <td>1 500 ₽</td>
+
+              <td>02.07.22</td>
+              <td>-</td>
+              <td className="act">Фото.png</td>
+              <td className="processing">В обработке</td>
+              <td>4 500 ₽</td>
+
+              <td>27.06.22</td>
+              <td>28.06.22</td>
+              <td className="act">Data.pdf</td>
+              <td className="paid">Выплачено</td>
+              <td>1 000 ₽</td>
+            </div>
           )}
         </div>
       </div>
