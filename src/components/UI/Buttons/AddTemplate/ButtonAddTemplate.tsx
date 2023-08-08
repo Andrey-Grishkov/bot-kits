591bee8666c1ff0d9698088a77dd1f51f12c68af
Андрей Гришков <andreygorm@gmail.com>
2023-08-08 14:10:42 +0300

    fix: button-add-template change to icon

--- src/components/UI/Buttons/AddTemplate/ButtonAddTemplate.tsx
+++ src/components/UI/Buttons/AddTemplate/ButtonAddTemplate.tsx
@@ -32,27 +32,27 @@ export const ButtonAddTemplate = ({
     return (
         <div className={cn(styles.buttonAddTemplate__wrapper, extraClass)}>
             <button className={styles.buttonAddTemplate} disabled={disabled} {...rest} >
-                <img className={styles.buttonAddTemplate__mainIcon} src={require(`./${icon}`)} alt="info-icon" />
-                <svg 
-                    className={styles.buttonAddTemplate__plusIcon} 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    width="24" 
-                    height="24" 
-                    viewBox="0 0 24 24" 
+                <img className={styles.buttonAddTemplate__mainIcon} src={icon} alt="info-icon" />
+                <svg
+                    className={styles.buttonAddTemplate__plusIcon}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
                     fill="none"
                 >
-                    <path 
-                        d="M12 5V19" 
-                        stroke={getColor(type)} 
-                        stroke-width="2" 
-                        stroke-linecap="round" 
+                    <path
+                        d="M12 5V19"
+                        stroke={getColor(type)}
+                        stroke-width="2"
+                        stroke-linecap="round"
                         stroke-linejoin="round"
                     />
-                    <path 
-                        d="M5 12H19" 
-                        stroke={getColor(type)} 
-                        stroke-width="2" 
-                        stroke-linecap="round" 
+                    <path
+                        d="M5 12H19"
+                        stroke={getColor(type)}
+                        stroke-width="2"
+                        stroke-linecap="round"
                         stroke-linejoin="round"
                     />
                 </svg>
