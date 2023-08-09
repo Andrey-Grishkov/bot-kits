d1b942576848b6586421d7eaa335834f97bd793e
Bashakin <bashakin@greensight.ru>
2023-08-10 02:29:54 +0300

    review fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -14,21 +14,24 @@ import Shared from "../Shared/Shared";
 import { ResetPassword } from "../ResetPassword/Reset-password";
 import { Mailing } from "../Mailing/Mailing";
 import { SubscriptionAndPayments } from "../SubscriptionAndPayments/SubscriptionAndPayments";
-import Overlay from "../NavBar/components/Overlay";
 
 function App() {
     const location = useLocation();
-    const isAuthPage = ['/reset-password', '/signup', '/signin'].includes(location.pathname);
-  
+    const isAuthPage = ["/reset-password", "/signup", "/signin"].includes(
+        location.pathname
+    );
+
     return (
-        <Overlay>
         <div className='page app-container'>
             {isAuthPage ? null : <Header />}
             {isAuthPage ? null : <NavBar />}
             <PageWrapper>
                 <Routes>
                     <Route path='/' element={<Dashboard />} />
-                    <Route path='/subscription_and_payments' element={<SubscriptionAndPayments />} />
+                    <Route
+                        path='/subscription_and_payments'
+                        element={<SubscriptionAndPayments />}
+                    />
                     <Route path='/addbot' element={<AddBotPage />} />
                     <Route path='/botbuilder' element={<BotBuilder />} />
                     <Route path='reset-password' element={<ResetPassword />} />
@@ -41,7 +44,6 @@ function App() {
             </PageWrapper>
             {isAuthPage ? null : <Footer />}
         </div>
-        </Overlay>
     );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,61 @@
 import "./Header.scss";
-import Burger from "../../images/icons/24/burger_menu.svg";
+import { ReactComponent as Burger } from "../../images/icons/24/burger_menu.svg";
 import Arrow from "../../images/icons/24/arrow_down.svg";
 import Avatar from "../../images/user_icon.png";
 import BellIcon from "../../images/icons/24/bell.svg";
 import HelpIcon from "../../images/icons/24/help.svg";
+import { useNav } from "../../context/navBarContext";
+import { useState, useEffect } from "react";
 
 function Header() {
+    const { isOverlayOpen, setIsOverlayOpen } = useNav();
+
+    const [widthOffset, setWidthOffset] = useState(getWidthOffset());
+
+    function getWidthOffset() {
+        const screenWidth = window.innerWidth;
+        if (screenWidth > 1440) {
+            const offset = (screenWidth - 1440) / 2;
+            return offset;
+        }
+        return 0;
+    }
+
+    useEffect(() => {
+        function handleResize() {
+            setWidthOffset(getWidthOffset());
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <header className='header'>
-            <img width='24px' height='24px' src={Burger} alt='Burger Menu' />
-            <div style={{ display: "flex", gap: "32px", alignItems: "center" }}>
+        <header className='header' style={{ marginRight: `-${widthOffset}px` }}>
+            <button
+                className={
+                    isOverlayOpen
+                        ? "header__burger-icon header__burger-icon_state_active"
+                        : "header__burger-icon"
+                }
+                onClick={() => {
+                    setIsOverlayOpen(!isOverlayOpen);
+                }}
+            >
+                <Burger />
+            </button>
+
+            <div
+                style={{
+                    display: "flex",
+                    gap: "32px",
+                    alignItems: "center",
+                    marginRight: `${widthOffset}px`,
+                }}
+            >
                 <p
                     className='icon-desktop'
                     style={{
