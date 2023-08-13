c178a6214ab393ae7ee2b58dd6859bfeed8fe9d5
randomu3 <demiz99@mail.ru>
2023-08-13 15:44:15 +0500

    added second mobile table

--- src/components/Shared/Shared.tsx
+++ src/components/Shared/Shared.tsx
@@ -214,45 +214,76 @@ const PayoutsTable: React.FC<PayoutsTableProps> = ({
           <img src={upImg} alt="up" />
         </button>
       </div>
+      
       {isPayoutsVisible && (
-        <div className="partners__table partners__table--payouts">
-          <table>
-            <thead>
-              <tr>
-                <th>Дата запроса</th>
-                <th>Дата выплаты</th>
-                <th>Акт</th>
-                <th>Статус</th>
-                <th>Сумма выплаты</th>
-              </tr>
-            </thead>
-            <tbody>
-              {mockPayouts.map((row, index) => (
-                <tr key={index}>
-                  <td>{row.requestDate}</td>
-                  <td>{row.paymentDate}</td>
-                  <td className="act">{row.act}</td>
-                  <td
-                    className={
-                      row.status === "Выплачено"
-                        ? "paid"
-                        : row.status === "В обработке"
-                        ? "processing"
-                        : ""
-                    }
-                  >
-                    {row.status}
-                  </td>
-                  <td>{row.payoutSum} ₽</td>
+        <>
+          {/* Мобильная версия */}
+          <div className="partners__table partners__table--payouts partners__table--mobile">
+            <table>
+              <tbody>
+                {mockPayouts.map((row, index) => (
+                  <React.Fragment key={index}>
+                    <tr key={"requestDate-" + index}>
+                      <th>Дата запроса</th>
+                      <td>{row.requestDate}</td>
+                    </tr>
+                    <tr key={"paymentDate-" + index}>
+                      <th>Дата выплаты</th>
+                      <td>{row.paymentDate}</td>
+                    </tr>
+                    <tr key={"act-" + index}>
+                      <th>Акт</th>
+                      <td>{row.act}</td>
+                    </tr>
+                    <tr key={"status-" + index}>
+                      <th>Статус</th>
+                      <td className={row.status === "Выплачено" ? "paid" : row.status === "В обработке" ? "processing" : ""}>
+                        {row.status}
+                      </td>
+                    </tr>
+                    <tr key={"payoutSum-" + index}>
+                      <th>Сумма выплаты</th>
+                      <td>{row.payoutSum} ₽</td>
+                    </tr>
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          {/* Десктопная версия */}
+          <div className="partners__table partners__table--payouts partners__table--desktop">
+            <table>
+              <thead>
+                <tr>
+                  <th>Дата запроса</th>
+                  <th>Дата выплаты</th>
+                  <th>Акт</th>
+                  <th>Статус</th>
+                  <th>Сумма выплаты</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {mockPayouts.map((row, index) => (
+                  <tr key={index}>
+                    <td>{row.requestDate}</td>
+                    <td>{row.paymentDate}</td>
+                    <td>{row.act}</td>
+                    <td className={row.status === "Выплачено" ? "paid" : row.status === "В обработке" ? "processing" : ""}>
+                      {row.status}
+                    </td>
+                    <td>{row.payoutSum} ₽</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
     </div>
   );
 };
+
 
 const Shared = () => {
   const [isPayoutsVisible, setPayoutsVisible] = useState(true);
